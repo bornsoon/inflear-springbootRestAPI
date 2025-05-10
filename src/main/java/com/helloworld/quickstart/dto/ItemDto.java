c0f62a5c6bca880cce15800ca9e4707d0e3cb654
bornsoon <hyolim922@hotmail.com>
2025-05-11 00:07:04 +0900

    item DTO 추가

--- src/main/java/com/helloworld/quickstart/dto/ItemDto.java
+++ src/main/java/com/helloworld/quickstart/dto/ItemDto.java
@@ -1,4 +1,23 @@
 package com.helloworld.quickstart.dto;
 
 public class ItemDto {
+    private String id;
+    private String name;
+
+    // Alt + Ins 누르면 자동 완성 메뉴
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
